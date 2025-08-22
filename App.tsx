e71a91c584da04bc8ef71fb08a4e7baeb116e910
cadudml2707 <dcaduardo@gmail.com>
2025-08-22 08:24:45 -0400

    Escopo inicial do projeto

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
 import './global.css';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { LoginView } from 'views/LoginView';
+import { MenuView } from 'views/MenuView';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <SafeAreaView className='flex-1'>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login'>
+          <Stack.Screen name="Login" component={LoginView} options={{ headerShown: false }} />
+          <Stack.Screen 
+            name="Menu" 
+            component={MenuView} 
+            options={{
+              headerStyle: {
+                backgroundColor: '#3b82f6',
+              },
+            }} 
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
